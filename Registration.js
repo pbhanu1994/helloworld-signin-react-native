bf42efe7d32bbe7f59576e61a62d081493aa6ac2
Bhanu <pbhanu.1994@gmail.com>
2018-08-18 05:56:23 +1000

    Modifications to the Register.js - Less code

--- Registration.js
+++ Registration.js
@@ -15,29 +15,30 @@ export default class Registration extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: '',
             email: '',
             password: '',
-        }
+        };
+        this.signUp = this.signUp.bind(this);
+        this.signIn = this.signIn.bind(this);
     }
 
-    updateValue(text, field) {
-        if (field == 'name' && field != "") {
-            this.setState({
-                name: text,
-            })
-        }
-        else if (field == 'email' && field != "") {
-            this.setState({
-                email: text,
-            })
-        }
-        else if (field == 'password' && field != "") {
-            this.setState({
-                password: text,
-            })
-        }
-    }
+    // handleInput(text, field) {
+    //     if (field == 'name' && field != "") {
+    //         this.setState({
+    //             name: text,
+    //         })
+    //     }
+    //     else if (field == 'email' && field != "") {
+    //         this.setState({
+    //             email: text,
+    //         })
+    //     }
+    //     else if (field == 'password' && field != "") {
+    //         this.setState({
+    //             password: text,
+    //         })
+    //     }
+    // }
 
     componentWillMount() {
         //Configurating firebase with api Key, authDomain, DB Name, etc:
@@ -46,18 +47,31 @@ export default class Registration extends Component {
 
     showToConsole = () => {
         if (this.state.name == "") {
-            Alert.alert("Please input the Name field")
+            Alert.alert("OOPS!","Please input the Name field")
         }
         else if (this.state.email == "") {
-            Alert.alert("Please input the Email field")
+            Alert.alert("OOPS!","Please input the Email field")
         }
         else if (this.state.password == "") {
-            Alert.alert("Please input the Password field")
+            Alert.alert("OOPS!","Please input the Password field")
         }
         //Passing the values to the regInfo object in Firebase.js:
         Firebase.regInfo.name = this.state.name,
         Firebase.regInfo.email = this.state.email,
-        Firebase.regInfo.password = this.state.password,
+        Firebase.regInfo.password = this.state.password
+    }
+
+    //Sign UP an User:
+    signUp = () => {
+        if (this.state.name == "") {
+            Alert.alert("OOPS!","Please input the Name field")
+        }
+        else if (this.state.email == "") {
+            Alert.alert("OOPS!","Please input the Email field")
+        }
+        else if (this.state.password == "") {
+            Alert.alert("OOPS!","Please input the Password field")
+        }
 
         //Creating User with email and password:
         Firebase.auth.createUserWithEmailAndPassword(this.state.email, this.state.password).catch(function (error) {
@@ -72,17 +86,44 @@ export default class Registration extends Component {
         });
     }
 
+    //Sign IN an User:
+    signIn = () => {
+        if (this.state.email == "") {
+            Alert.alert("OOPS!","Please input the Email field")
+        }
+        else if (this.state.password == "") {
+            Alert.alert("OOPS!","Please input the Password field")
+        }
+
+        //Creating User with email and password:
+        Firebase.auth.signInWithEmailAndPassword(this.state.email, this.state.password).catch(function (error) {
+            //Handling errors:
+            var errorCode = error.code;
+            var errorMessage = error.Message;
+
+            if (errorCode == 'auth/weak-password') {
+                alert('The password is too weak.');
+            }
+            console.log(error);
+        });
+    }
+
     render() {
         return (
             <View style={styles.registrationForm}>
-                <Text style={styles.header}>Registration Form</Text>
-
-                <TextInput style={styles.textinput} placeholder="Your Name" onChangeText={(text) => this.updateValue(text, 'name')} value={this.state.name}></TextInput>
-                <TextInput style={styles.textinput} placeholder="Your Email" onChangeText={(text) => this.updateValue(text, 'email')} value={this.state.email}></TextInput>
-                <TextInput style={styles.textinput} secureTextEntry={true} placeholder="Your Passwsord" onChangeText={(text) => this.updateValue(text, 'password')} value={this.state.password}></TextInput>
-                <TouchableOpacity style={styles.button} onPress={this.showToConsole}>
+                <Text style={styles.header}>Register User</Text>
+                <TextInput style={styles.textinput} placeholder="Your Email" placeholderTextColor="#fff" onChangeText={(text) => this.setState({email: text})} defaultValue={this.state.email}></TextInput>
+                <TextInput style={styles.textinput} secureTextEntry={true} placeholder="Your Passwsord" placeholderTextColor="#fff" onChangeText={(text) => this.setState({password: text})} defaultValue={this.state.password}></TextInput>
+                <TouchableOpacity style={styles.button} onPress={this.signUp}>
                     <Text style={styles.btntext}>Sign Up</Text>
                 </TouchableOpacity>
+                <Text style={styles.divideText}> OR 
+                    
+                </Text>
+                <Text style={{color: 'cyan',textAlign:'center', paddingTop:5, fontWeight:'bold'}}
+                    onPress={this.signIn}>
+                        Sign In
+                </Text>
             </View>
         );
     }
@@ -99,7 +140,8 @@ const styles = StyleSheet.create({
         paddingBottom: 10,
         marginBottom: 40,
         borderBottomColor: '#199187',
-        borderBottomWidth: 1
+        borderBottomWidth: 1,
+        textAlign:'center'
     },
     textinput: {
         alignSelf: 'stretch',
@@ -108,6 +150,14 @@ const styles = StyleSheet.create({
         color: '#fff',
         borderBottomColor: '#f8f8f8',
         borderBottomWidth: 1,
+        alignItems:'center',
+
+    },
+    signInBtn:{
+        color: 'cyan',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingTop: 5
     },
     button: {
         alignSelf: 'stretch',
@@ -118,7 +168,14 @@ const styles = StyleSheet.create({
     },
     btntext: {
         color: '#fff',
-        fontWeight: 'bold'
+        fontWeight: 'bold',
+        
+    },
+    divideText:{
+        color: '#fff',
+        fontWeight: 'bold',
+        textAlign: 'center',
+        paddingTop: 20
     }
 });
 
